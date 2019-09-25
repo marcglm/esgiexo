cf2dd91a46c35ff4d49d778e5071467a60ce4f43
Marc <marcghalem@hotmail.fr>
2019-09-25 14:31:18 +0200

    version 2.0

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-
-const express = require('express') //utilisation d'express
+ const express = require('express') //utilisation d'express
 const app = express() //Initialisation d'express
 
 app.get('/', function (req, res) {  //
   res.send('Hello World!')
 })
 
-app.listen(3000, function () { // ecoute sur le port 3000 
+app.get('/hello', function (req, res) {  //
+console.log('params', req.query.nom)
+if (req.query.nom){
+	res.send(`Bonjour , ${req.query.nom} !`)
+}else{
+	res.send('Quelle est votre nom?')
+
+}
+
+})
+app.listen(process.env.PORT||3000, function () { // ecoute sur le port 3000 
   console.log('Example app listening on port 3000!')
 })
