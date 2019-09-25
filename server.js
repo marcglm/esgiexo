159c03e7798ecb337d63fbe22d68d74da1b69045
Marc <marcghalem@hotmail.fr>
2019-09-25 11:40:56 +0200

    Nouvelle version du fichier server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-alert("Hello, World!");
+
+const express = require('express') //utilisation d'express
+const app = express() //Initialisation d'express
+
+app.get('/', function (req, res) {  //
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () { // ecoute sur le port 3000 
+  console.log('Example app listening on port 3000!')
+})
